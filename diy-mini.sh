aa1302c26d29dace77b98a4901374fde5939eef2
kxdn3 <51870018+kxdn3@users.noreply.github.com>
2024-01-10 19:33:56 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -23,9 +23,10 @@ rm -rf feeds/luci/applications/luci-app-netdata
 # 添加额外插件
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
-git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+# git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
 # svn export https://github.com/syb999/openwrt-19.07.1/trunk/package/network/services/msd_lite package/msd_lite
 git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
+git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
 
 # 科学上网插件
 git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
