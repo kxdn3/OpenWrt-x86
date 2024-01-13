ac0a4095890d5822dcf2d37764316d4fca32f02c
kxdn3 <51870018+kxdn3@users.noreply.github.com>
2024-01-13 14:01:32 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -34,6 +34,7 @@ function git_sparse_clone() {
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
 # git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
+git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
 
 # 科学上网插件
 git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
