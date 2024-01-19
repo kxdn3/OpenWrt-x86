f9c40fb49aecdb1c7217ed702ac55877eeb4a495
kxdn3 <51870018+kxdn3@users.noreply.github.com>
2024-01-19 22:49:57 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -62,7 +62,7 @@ git_sparse_clone main https://github.com/haiibo/packages luci-theme-atmaterial l
 cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
 # 更改默认主题
-# sed -i 's/luci-theme-bootstrap/luci-theme-opentomcat/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-opentomcat/g' ./feeds/luci/collections/luci/Makefile
 
 # 晶晨宝盒
 git_sparse_clone main https://github.com/ophub/luci-app-amlogic luci-app-amlogic
