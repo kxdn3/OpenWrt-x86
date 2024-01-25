d3f060dc375009b1c9e0b6747b775cd483305927
kxdn3 <51870018+kxdn3@users.noreply.github.com>
2024-01-25 18:31:14 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -3,7 +3,7 @@
 # 修改默认IP
 # sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
-# 更改默认 Shell 为 zsh
+# 更改默认 Shell 为 zs
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # TTYD 免登录
