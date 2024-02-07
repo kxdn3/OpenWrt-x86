e568c8de3fa0fdc1bc1b63257f9a20b1af38f727
kxdn3 <51870018+kxdn3@users.noreply.github.com>
2024-02-07 12:35:15 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -20,9 +20,6 @@ rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-netdata
 rm -rf feeds/packages/net/ddns-scripts
-rm -rf package/lean/ddns-scripts_aliyun
-rm -rf package/lean/ddns-scripts_dnspod
-rm -rf  package/amdgpu-firmware
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
