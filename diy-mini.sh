1bc91d55235f33ba9b7aa5b6a2df185ee410d868
kxdn3 <51870018+kxdn3@users.noreply.github.com>
2024-03-09 21:22:30 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -40,10 +40,10 @@ git clone https://github.com/kxdn3/luci-app-pushbot package/luci-app-pushbot
 
 #  科学上网插件
 git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
-git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
-git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
+# git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+# git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+# git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
 # Themes
 # git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
