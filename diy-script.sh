695d3ede5cd2080e4ed38e407b41de994ff47ead
kxdn3 <51870018+kxdn3@users.noreply.github.com>
2024-03-12 12:18:45 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -114,3 +114,11 @@ find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+
+# 移除 openwrt feeds 自带的核心包
+rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
+git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
+
+# 更新 golang 1.22 版本
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
