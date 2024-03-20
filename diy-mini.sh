e45007431df3c08e802883a1b9a961cbd40796f1
kxdn3 <51870018+kxdn3@users.noreply.github.com>
2024-03-21 01:08:42 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -60,8 +60,8 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages packa
 
 # Themes
 # git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
-# git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-# git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 # git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
 # git_sparse_clone main https://github.com/haiibo/packages luci-theme-opentomcat
 git clone https://github.com/y9858/luci-theme-opentomcat package/luci-theme-opentomcat
