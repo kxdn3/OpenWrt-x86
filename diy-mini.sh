fa054a064a3d8c8ad1d32302803a6636904b0f2f
kxdn3 <51870018+kxdn3@users.noreply.github.com>
2024-03-21 01:01:35 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -67,7 +67,7 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages packa
 git clone https://github.com/y9858/luci-theme-opentomcat package/luci-theme-opentomcat
 
 # 更改 Argon 主题背景
-cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+# cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
 # 更改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-opentomcat/g' ./feeds/luci/collections/luci/Makefile
