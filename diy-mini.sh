1086fbbaa4352db1d86fe54cb7e22a2d501ddd25
kxdn3 <51870018+kxdn3@users.noreply.github.com>
2024-03-29 13:26:44 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -29,8 +29,6 @@ rm -rf feeds/luci/applications/luci-app-netdata
 rm -rf feeds/luci/applications/luci-app-pushbot
 rm -rf feeds/luci/applications/luci-app-dockerman
 rm -rf feeds/luci/applications/luci-app-diskman
-rm -rf feeds/packages/amdgpu-firmware
-rm -rf feeds/package/amdgpu-firmware
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
