2075013ff29bfea5446c3a4f255bf87c1699a95b
kxdn3 <51870018+kxdn3@users.noreply.github.com>
2024-04-04 18:22:58 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -10,7 +10,7 @@ sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # 改为6.6内核
-sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+# sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 
 # 移除要替换的包
 rm -rf feeds/packages/net/mosdns
