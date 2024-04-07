f66f9c1ee9f016dad42112860003b45b29b9ab60
kxdn3 <51870018+kxdn3@users.noreply.github.com>
2024-04-07 12:00:26 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -137,4 +137,4 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_U
 
 # 移除 openwrt feeds 自带的核心包
 rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
-git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
+# git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
