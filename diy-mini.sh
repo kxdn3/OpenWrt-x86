f90863d01eaa58a9d04fa7f1a89e475876fff9b4
kxdn3 <51870018+kxdn3@users.noreply.github.com>
2024-04-09 19:42:23 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -59,8 +59,8 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-a
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/simple-
 
 # 更新haproxy
-# rm -rf feeds/packages/net/haproxy
-# git clone https://github.com/openwrt/packages package/net/haproxy
+rm -rf feeds/packages/net/haproxy
+git clone https://github.com/openwrt/packages package/net/haproxy
 
 # 更新curl版本
 rm -rf feeds/packages/net/curl
