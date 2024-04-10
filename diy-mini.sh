ae275da46e337bf545a7ee45ab49760b1b1323b3
kxdn3 <51870018+kxdn3@users.noreply.github.com>
2024-04-10 14:47:21 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -151,7 +151,7 @@ sed -i 's/luciversion)%>)/luciversion)%>)-->/g' package/lean/autocore/files/*/in
 # 修改版本为编译日期
 date_version=$(date +"%y.%m.%d")
 orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-sed -i "s/${orig_version}/R${date_version} by kx/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/${orig_version}/R${date_version} by kxdn/g" package/lean/default-settings/files/zzz-default-settings
 
 # 取消默认密码
 sed -i '/\/etc\/shadow/{/root/d;}' package/lean/default-settings/files/zzz-default-settings
